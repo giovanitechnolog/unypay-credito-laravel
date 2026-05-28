5a0b4ff714088cbe27cb5fb5b981e3989e4ec958
giovanitechnolog <giovani@technolog.com.br>
2026-05-28 09:43:02 -0300

    chore: atualiza DatabaseSeeder com a esteira unificada

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,20 +6,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
+        // 🚀 Execução sequencial e limpa de toda a carga do sistema
         $this->call([
+            // 1. Cadastra o usuário administrador do sistema
             AdminUserSeeder::class,
-        ]);
 
-        $this->call([
-            ContractTypeSeeder::class,
-        ]);
+            // 2. Cria as categorias base (Mútuo, Consignado...)
+            ContractTypeSeeder::class, 
 
-        $this->call([
-            ContractTypeSeeder::class, // Tipos (Mútuo, Consignado...)
-            LoadContractsDataSeeder::class, // 👈 Nova Linha Injetada Aqui!
-        ]);
+            // 3. Injeta as cargas de Auditoria Purificada dos PDFs (14 Contratos Legítimos)
+            LoadExternalContractsSeeder::class, 
 
+            // 4. Injeta as baixas financeiras históricas, datas e valores reais da Planilha
+            PopuleEcosistemaPlanilhaSeeder::class, 
+        ]);
     }
 }
